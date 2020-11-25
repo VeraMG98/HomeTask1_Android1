ba2ed2f4592d07792b643d61c51eac6ef76e6b45
VeraMG98 <rusyamg@mail.ru>
2020-11-25 10:38:42 +0300

    innit 2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,36 +13,32 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button button_sing_in;
     EditText login, password;
+    Button button_sing_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         inElements();
-        singIn();
-
-    }
-
-
-    public void singIn(){
-        button_sing_in.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (login.getText().length() == 0 || password.getText().length() == 0)
-                    Toast.makeText(MainActivity.this, "Enter login or password", Toast.LENGTH_SHORT).show();
-                else
-                    Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 
     public void inElements(){
         login = findViewById(R.id.login);
-        login.setText("");
         password = findViewById(R.id.password);
-        password.setText("");
         button_sing_in = findViewById(R.id.button);
+
+        button_sing_in.setOnClickListener(v -> {
+            String text1 = login.getText().toString().trim();
+            String text2 = password.getText().toString().trim();
+            if (text1.equals("") || text2.equals("")) {
+                Toast.makeText(MainActivity.this, "Enter login or password", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+            }
+            if (text2.length() > 6) {
+                Toast.makeText(MainActivity.this, "Enter correct password", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
